81c843e2e6856fe5f3f30e3b11f95145bffb344f
Mathias Slawik <mathias.slawik@tu-berlin.de>
2013-11-05 12:09:57 +0100

    First commit

---
+++ app/controllers/service_controller.rb
@@ -0,0 +1,37 @@
+require 'sdl'
+
+class ServiceController < ApplicationController
+  def list
+    @services = compendium.services
+  end
+
+  def show
+  end
+
+  private
+    def compendium
+      @compendium ||= ServiceController.build_compendium
+    end
+
+    def self.build_compendium
+      compendium = SDL::Base::ServiceCompendium.new
+
+      sdl_gem_dir = Gem::Specification.find_by_name('sdl-ng').gem_dir
+
+      # Load SDL
+      Dir.glob(File.join(sdl_gem_dir, 'examples', '**', '*.sdl.rb')) do |filename|
+        compendium.facts_definition do
+          eval(File.read(filename), binding, filename)
+        end
+      end
+
+      # Load Service Definitions
+      Dir.glob(File.join(sdl_gem_dir, 'examples', '**', '*.service.rb')) do |filename|
+        compendium.service filename.match(%r[.+/(.+).service.rb])[1] do
+          eval(File.read(filename), binding, filename)
+        end
+      end
+
+      compendium
+    end
+end
