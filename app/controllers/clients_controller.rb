914ef809bde99cd6cfea304c27379d9ef3b66efd
Mathias Slawik <mathias.slawik@tu-berlin.de>
2014-08-06 16:28:35 +0200

    Add ClientsController#put and ClientsController#delete

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -56,7 +56,7 @@ class ClientsController < ApplicationController
   param :client_data, String, :desc => 'Arbitrary client data', :required => false
   param :client_profile, String, :desc => 'Client search profile', :required => false
   description <<-END
-    On successful invocation, the method returns the HTTP status code `201 Created` with the URL of the client as the HTTP `Location` header.
+    On successful completion, the method returns the HTTP status code `201 Created` with the URL of the client as the HTTP `Location` header.
   END
   error 500, 'Server error while creating client'
   def create
@@ -66,17 +66,52 @@ class ClientsController < ApplicationController
           :client_profile => params[:client_profile]
       )
 
-      render text: 'Created', location: client_url(client), status: 201
+      head :created, location: client_url(client)
     rescue Mongoid::Errors::MongoidError => e
       render text: "Error creating client: #{e.message}", status: 500
     end
   end
 
-  def update
+  api :PUT, 'clients/:id', 'Updates or creates a new client with the specified id'
+  param :client_data, String, :desc => 'Arbitrary client data', :required => false
+  param :client_profile, String, :desc => 'Client search profile', :required => false
+  description <<-END
+    If the client does not exist yet, the method creates a new client and returns `201 Created`.
 
+    If the client already exists, the method updates `client_data` and `client_profile` if specified and returns `204 No Content`.
+  END
+  error 422, 'ID missing or invalid'
+  error 500, 'Internal server error while creating or updating client'
+  def update
+    if params[:id]
+      client_attributes = {}
+      client_attributes['client_data'] = params[:client_data] if params[:client_data]
+      client_attributes['client_profile'] = params[:client_profile] if params[:client_profile]
+
+      operation_result = Client.collection.find(_id: params[:id]).update({'$set' => client_attributes}, [:upsert])
+
+      if operation_result['updatedExisting']
+        head :no_content
+      else
+        head :created
+      end
+    else
+      render text: 'ID missing or invalid', status: 422
+    end
   end
 
-  def delete
+  api :DELETE, 'client/:id', 'Deletes a client'
+  description <<-END
+    On successful completion, the method returns the HTTP status code `204 No Content`.
+  END
+  error 404, 'The client does not exist'
+  def destroy
+    begin
+      Client.find(params[:id]).destroy
 
+      head :no_content
+    rescue Mongoid::Errors::DocumentNotFound
+      render text: 'Client not found', status: 404
+    end
   end
 end
--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe ClientsController do
+
   describe 'GET #index' do
     include_context 'with existing clients'
 
@@ -45,6 +46,64 @@ describe ClientsController do
       expect(response).to be_success
       expect(response.status).to eq(201)
       expect(response['Location']).to eq(client_url(Client.first))
+    end
+  end
+
+  describe 'PUT #update' do
+    after(:each) do
+      Client.delete_all
+    end
+
+    it 'creates a new client and responds with 201 if the client did not exist' do
+      put :update, :id => 'new-id', :client_data => 'client data', :client_profile => 'client profile'
+
+      expect(Client.find('new-id').client_data).to eq 'client data'
+
+      expect(response).to be_success
+      expect(response.status).to eq 201
+    end
+
+    it 'updates an existing client and responds with 204' do
+      client = Client.create(:client_data => 'old', :client_profile => 'old')
+
+      put :update, :id => client._id, :client_data => 'new'
+
+      client.reload
+
+      expect(response).to be_success
+      expect(response.status).to eq 204
+
+      expect(client.client_data).to eq 'new'
+      expect(client.client_profile).to eq 'old'
+    end
+
+    it 'responds with 422 if the id was missing' do
+
+    end
+  end
+
+  describe 'DELETE #delete' do
+    after(:each) do
+      # If the test fails, leave a clean database
+      Client.delete_all
+    end
+
+    it 'deletes a client and returns 204' do
+      client = Client.create
+
+      delete :destroy, id: client._id
+
+      expect(response).to be_success
+      expect(response.status).to eq(204)
+
+      expect {Client.find(client._id)}.to raise_exception Mongoid::Errors::DocumentNotFound
+    end
+
+    it 'returns a 404 error if the client cannot be found' do
+      delete :destroy, id: 'ABC'
+
+      expect(response).to be_missing
+      expect(response.status).to eq(404)
     end
   end
 end
