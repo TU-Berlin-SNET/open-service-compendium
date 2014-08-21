df205d936239c459cdb34a056f8c496de6b70586
Mathias Slawik <mathias.slawik@tu-berlin.de>
2014-08-21 17:12:56 +0200

    Added more general documentation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -24,7 +24,7 @@ module Apipie::Helpers
 end
 
 Apipie.configure do |config|
-  config.app_name                = "OpenServiceBroker"
+  config.app_name                = 'Open Service Broker'
   config.api_base_url            = "/"
   config.doc_base_url            = "/apipie"
   # where is your API defined?
@@ -32,4 +32,23 @@ Apipie.configure do |config|
   config.markup                  = Apipie::Markup::Kramdown.new
   config.reload_controllers      = true
   config.validate                = false
+  config.app_info = <<-END
+# TRESOR Open Service Broker
+
+## Main functionality
+
+The TRESOR Open Service Broker is an information system realizing the following main functions:
+
+* Managing service descriptions, clients, and providers
+* Performing booking of services
+
+## Format of parameters
+
+All described parameters can be sent using these methods:
+
+* Sending parameters within the HTTP body, `multipart/form-data` encoded (on POST & PUT methods)
+* Sending parameters as a query string (on all methods)
+
+Both methods can be combined, e.g. sending one parameter in the query string and another as `multipart/form-data`.
+  END
 end
